9c600c4bd7eace9e5044976f33f4362a6eee88d2
ellagoris <eduardofidera@gmail.com>
2022-10-11 09:03:46 -0300

    feat: add create_character instruction

--- programs/rpg-program/src/lib.rs
+++ programs/rpg-program/src/lib.rs
@@ -1,4 +1,6 @@
-use anchor_lang::prelude::*;
+use std::mem::size_of;
+
+use anchor_lang::{ prelude::* };
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -6,10 +8,62 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod rpg_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_character(ctx: Context<CreateCharacter>, name: String) -> Result<()> {
+        let character = CharacterAccount::new(&name)?;
+
+        ctx.accounts.character.set_inner(character);
+
         Ok(())
     }
 }
 
+#[account]
+#[derive(Default)]
+pub struct CharacterAccount {
+    pub name: String,
+    pub experience: u32,
+}
+
+const NAME_MAX_LENGTH: usize = 16;
+
+impl CharacterAccount {
+    pub fn space() -> usize {
+        8 + size_of::<Self>()
+    }
+
+    pub fn new(name: &str) -> Result<Self> {
+        require!(name.len() <= NAME_MAX_LENGTH, CharacterError::MaxNameLengthExceeded);
+
+        let account = CharacterAccount {
+            name: name.to_string(),
+            experience: 0,
+            ..Default::default()
+        };
+
+        Ok(account)
+    }
+}
+
+pub const PREFIX: &str = "character";
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateCharacter<'info> {
+    #[account(
+        init,
+        space = CharacterAccount::space(),
+        payer = owner,
+        seeds = [PREFIX.as_ref(), owner.key().as_ref()],
+        bump
+    )]
+    pub character: Account<'info, CharacterAccount>,
+    /// Character owner keypair.
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[error_code]
+pub enum CharacterError {
+    #[msg("Name is too long. Max. length is 16 bytes.")]
+    MaxNameLengthExceeded,
+}
--- tests/rpg-program.ts
+++ tests/rpg-program.ts
@@ -1,6 +1,17 @@
 import * as anchor from "@project-serum/anchor"
 import { Program } from "@project-serum/anchor"
+import { expect } from "chai"
 import { RpgProgram } from "../target/types/rpg_program"
+
+const getCharacterAddress = (
+  owner: anchor.web3.PublicKey,
+  charactersProgram: anchor.web3.PublicKey
+): anchor.web3.PublicKey => {
+  return anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("character"), owner.toBuffer()],
+    charactersProgram
+  )[0]
+}
 
 describe("rpg-program", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +19,28 @@ describe("rpg-program", () => {
 
   const program = anchor.workspace.RpgProgram as Program<RpgProgram>
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc()
+  it("Can create a character", async () => {
+    const character = getCharacterAddress(
+      program.provider.publicKey,
+      program.programId
+    )
+    const owner = program.provider.publicKey
+    const systemProgram = anchor.web3.SystemProgram.programId
+
+    const tx = await program.methods
+      .createCharacter("test")
+      .accounts({
+        character,
+        owner,
+        systemProgram,
+      })
+      .rpc()
+
+    const accountInfo = await program.account.characterAccount.fetch(character)
+    console.log(accountInfo)
+
+    expect(accountInfo.name).to.eq("test")
+
     console.log("Your transaction signature", tx)
   })
 })
