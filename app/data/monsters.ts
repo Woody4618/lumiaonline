f097b5da6fa52987caa2d9e70d17b0cf1cfa075c
ellagoris <eduardofidera@gmail.com>
2022-11-01 18:01:44 -0300

    feat: change program fodler name

--- app/data/monsters.ts
+++ app/data/monsters.ts
@@ -5,6 +5,7 @@ export const monsters = [
     image:
       "https://replicate.com/api/models/stability-ai/stable-diffusion/files/9de950c5-a1a5-4cba-8bb6-1a0d98b58534/out-0.png",
     meleeSkill: 20,
+    spawn: 10,
   },
   {
     name: "Fish",
@@ -12,6 +13,7 @@ export const monsters = [
     image:
       "https://replicate.com/api/models/stability-ai/stable-diffusion/files/ab2813cc-843c-4235-9aec-70109e52522e/out-0.png",
     meleeSkill: 10,
+    spawn: 10,
   },
   {
     name: "Hyaena",
@@ -19,5 +21,6 @@ export const monsters = [
     image:
       "https://replicate.com/api/models/stability-ai/stable-diffusion/files/aa8b9dad-e17b-4e46-9436-c5eaf35c2526/out-0.png",
     meleeSkill: 20,
+    spawn: 10,
   },
 ]
