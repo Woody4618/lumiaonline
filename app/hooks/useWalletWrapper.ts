b044a14d5230e89ccb0d037e3fc5d0ce1770b7e1
ellagoris <eduardofidera@gmail.com>
2022-11-03 22:19:26 -0300

    feat: check if wallet name is valid

--- app/hooks/useWalletWrapper.ts
+++ app/hooks/useWalletWrapper.ts
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react"
 const useWalletWrapper = () => {
   const walletContetState = useWallet()
 
-  const { publicKey, wallet, autoConnect } = walletContetState
+  const { publicKey, wallet, autoConnect, wallets } = walletContetState
   /** Boolean for whether the wallet provider has finished running or not. */
   const [isWalletReady, setIsWalletReady] = useState(false)
 
@@ -17,9 +17,28 @@ const useWalletWrapper = () => {
   useEffect(() => {
     const walletName = localStorage.getItem("walletName")
 
+    const availableWallets = wallets.map((wallet) =>
+      wallet.adapter.name.toString()
+    )
+
+    /**
+     * Here `walletName` can be set from another chain.
+     *
+     * For example, Aptos uses it through their wallet providers
+     * So we try to ignore unexpected wallet names.
+     * */
+    const isWalletValid = walletName
+      ? availableWallets.indexOf(walletName) === -1
+        ? false
+        : true
+      : true
+
     if (autoConnect && walletName && publicKey) {
       setIsWalletReady(true)
     } else if (!walletName || !autoConnect) {
+      setIsWalletReady(true)
+    } else if (!isWalletValid) {
+      /** If the wallet is invalid, set the state ready so the user can select a new one */
       setIsWalletReady(true)
     }
   }, [publicKey, wallet, autoConnect])
