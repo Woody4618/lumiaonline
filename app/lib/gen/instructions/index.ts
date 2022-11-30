cca0643c9c4fa107ca3a0ca4949346276ed7a4dd
ellagoris <eduardofidera@gmail.com>
2022-11-30 00:22:03 -0300

    feat: update lib gen

--- app/lib/gen/instructions/index.ts
+++ app/lib/gen/instructions/index.ts
@@ -15,11 +15,9 @@ export type {
   CreateMonsterSpawnArgs,
   CreateMonsterSpawnAccounts,
 } from "./createMonsterSpawn"
-export { killSpawn } from "./killSpawn"
-export type { KillSpawnAccounts } from "./killSpawn"
+export { joinBattle } from "./joinBattle"
+export type { JoinBattleArgs, JoinBattleAccounts } from "./joinBattle"
 export { joinQuest } from "./joinQuest"
 export type { JoinQuestAccounts } from "./joinQuest"
 export { claimQuest } from "./claimQuest"
 export type { ClaimQuestAccounts } from "./claimQuest"
-export { joinBattle } from "./joinBattle"
-export type { JoinBattleArgs, JoinBattleAccounts } from "./joinBattle"
